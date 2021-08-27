27075a913b16fc1006f38bc9d5cf9d801fe10d42
AshwiniSetty <ashwinisetty@hotmail.com>
2021-08-27 23:16:28 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cash_Register_Manager
+An website that tells, the minimum number of notes to return to the customer, when the bill amount and the cash given is entered.
---
+++ index.js
@@ -0,0 +1,46 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".notes");
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      showMessage("Amount to be returned is " + amountToBeReturned);
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage("Error! Invalid Cash Amount Received");
+    }
+  }else{showMessage("Error! Invalid Bill Amount");
+  }
+});
+
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
+
+function nextFunction(){
+  document.getElementById("next").style.display = "block";
+}
+
+function checkFunction(){
+  document.getElementById("check").style.display = "block";
+}
